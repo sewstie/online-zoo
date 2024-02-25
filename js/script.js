8cc0f7468faef072ed010622ba5264efc35fc135
Vladislav <vladislav.hushcha@gmail.com>
2024-02-25 18:27:55 +0300

    addex animation to logo for hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 window.onload = function () {
     const menu_btn = document.querySelector('.header__menu-burger')
-    const mobile_menu = document.querySelector('.header__menu-list-mobile')
+    const mobile_logo = document.querySelector('.header__top-logo')
 
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('active');
+        mobile_logo.classList.toggle('active');
     });
 }

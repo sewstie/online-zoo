35ce83f967cd7577c6d01efbcc53e7da868f30d3
Vladislav <vladislav.hushcha@gmail.com>
2024-02-25 18:45:50 +0300

    fixes + changed classes for nav menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 window.onload = function () {
     const menu_btn = document.querySelector('.header__menu-burger')
     const mobile_logo = document.querySelector('.header__top-logo')
+    const mobile_menu = document.querySelector('.header__menu-list-mobile')
 
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('active');
         mobile_logo.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
     });
 }

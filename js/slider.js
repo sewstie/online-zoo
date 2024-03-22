71289616095942eb821278d2dcc9a1a9f2a8dc24
Gusenov <iserveriazzotov22@gmail.com>
2024-03-22 18:40:19 +0300

    improved slider.js added grid extension

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+var splide = new Splide( '.splide', {
+    grid: {
+        rows: 2,
+        cols: 2,
+        gap : {
+            row: '20px',
+            col: '20px',
+        },
+    },
+});
+splide.mount(window.splide.Extensions);

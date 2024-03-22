13aa7207807eefbe4de999ad4c791adcb32db45e
Vladislav <vladislav.hushcha@gmail.com>
2024-03-22 19:54:37 +0300

    fixed slider without responsiveness

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var splide = new Splide( '.splide', {
-        type    : 'loop',
+
+    var splide1 = new Splide( '#splide1', {
+        type   : 'loop',
         arrows: false,
-        grid: {
-            rows: 2,
-            cols: 3,
-            gap : {
-                row: '30px',
-                col: '30px',
-            },
-      },
-    });
-    splide.mount(window.splide.Extensions);
+        pagination: false,
+        perPage: 3,
+        focus  : 0,
+        omitEnd: true,
+        gap: '28px',
+      } );
+      
+      splide1.mount();
+
+      var splide2 = new Splide( '#splide2', {
+        type   : 'loop',
+        arrows: false,
+        pagination: false,
+        perPage: 3,
+        focus  : 0,
+        omitEnd: true,
+        gap: '28px',
+      } );
+      
+      splide2.mount();
 
     btnNext.addEventListener('click', e => {
-        splide.go('+1')
+        splide1.go('+1')
+        splide2.go('+1')
       })
     
-      btnPrev.addEventListener('click', e => {
-        splide.go('-1')
+    btnPrev.addEventListener('click', e => {
+        splide1.go('-1')
+        splide2.go('-1')
       })
     
     });

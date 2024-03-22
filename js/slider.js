affbd8d8b7e20b93ea19433c652f9ecc89058635
Gusenov <iserveriazzotov22@gmail.com>
2024-03-22 18:50:11 +0300

    finished slider

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,12 @@
 var splide = new Splide( '.splide', {
+    type    : 'loop',
     grid: {
-        rows: 2,
-        cols: 2,
-        gap : {
-            row: '20px',
-            col: '20px',
-        },
-    },
+		rows: 2,
+		cols: 3,
+		gap : {
+            row: '30px',
+            col: '30px',
+		},
+  },
 });
 splide.mount(window.splide.Extensions);

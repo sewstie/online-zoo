928bda69ada78cda34174b5add1b30a05fd2e860
Gusenov <iserveriazzotov22@gmail.com>
2024-03-22 19:08:30 +0300

    added functional btn

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,24 @@
-var splide = new Splide( '.splide', {
-    type    : 'loop',
-    grid: {
-		rows: 2,
-		cols: 3,
-		gap : {
-            row: '30px',
-            col: '30px',
-		},
-  },
-});
-splide.mount(window.splide.Extensions);
+document.addEventListener('DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+        type    : 'loop',
+        arrows: false,
+        grid: {
+            rows: 2,
+            cols: 3,
+            gap : {
+                row: '30px',
+                col: '30px',
+            },
+      },
+    });
+    splide.mount(window.splide.Extensions);
+
+    btnNext.addEventListener('click', e => {
+        splide.go('+1')
+      })
+    
+      btnPrev.addEventListener('click', e => {
+        splide.go('-1')
+      })
+    
+    });
